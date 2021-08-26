da3310612539275c3a37b598defc340d00a4fdce
Yuliashka <laramera@outlook.it>
2021-08-26 08:45:14 +0200

    rev25._Revision for Heroku (using gmail app password at heroku)

--- main.py
+++ main.py
@@ -38,10 +38,9 @@ import smtplib
 from functools import wraps
 
 # VARIABLES:
-# MY_EMAIL = os.environ.get("MY_EMAIL")
-# MY_PASSWORD = os.environ.get("MY_PASSWORD")
-MY_EMAIL = "WantToTestMyApp@gmail.com"
-MY_PASSWORD = "vstalaiposhla123"
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
+
 
 app = Flask(__name__)
 
@@ -60,10 +59,8 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
